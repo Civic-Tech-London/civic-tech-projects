cdf285ce27fa19d3492b863182401050bbe3c578
Raghul Ramakrishnan <mail.me.raghul87@gmail.com>
2024-10-17 19:44:49 -0400

    Added Profile picture and few lines of bio

--- civic-tech-club/src/components/HoldProfiles.jsx
+++ civic-tech-club/src/components/HoldProfiles.jsx
@@ -21,10 +21,10 @@ const HoldProfiles = () => {
                     <Profile name="Marty Penner" image="/img/blank-profile.png" shapeNum = "3" blurb = "Est autem assumenda quas et ex in illum. Provident ipsa voluptate ducimus tempore mollitia animi dolorem. Quas corrupti quia dolorem eos est. Blanditiis libero maxime qui est."/>
                 </Col>
                 <Col xs={12} lg={6}>
-                    <Profile name="Raghul Ramakrishnan" image="/img/blank-profile.png" shapeNum = "4" blurb = "Est autem assumenda quas et ex in illum. Provident ipsa voluptate ducimus tempore mollitia animi dolorem. Quas corrupti quia dolorem eos est. Blanditiis libero maxime qui est."/>
+                    <Profile name="Raghul Ramakrishnan" image="/img/raghul.jpg" shapeNum = "4" blurb = "Est autem assumenda quas et ex in illum. Provident ipsa voluptate ducimus tempore mollitia animi dolorem. Quas corrupti quia dolorem eos est. Blanditiis libero maxime qui est."/>
                 </Col>
                 <Col xs={12} lg={6}>
-                    <Profile name="Jennifer Sadler" image="/img/jen-alpaca.jpg" shapeNum = "5" blurb = "Jen is a Web Developer for the Faculty of Education at Western University. She started this group to try and make a difference in London. She’s very involved in the Unitarian Fellowship of London."/>
+                    <Profile name="Jennifer Sadler" image="/img/jen-alpaca.jpg" shapeNum = "5" blurb = "Raghul Ramakrishnan is a versatile professional skilled in both front-end and back-end technologies. rted this group to try and make a difference in London. She’s very involved in the Unitarian Fellowship of London."/>
                 </Col>
             </Row>
         </Container>
