75f6ae298c7982b24c911589f26ca53260cfe98a
Raghul Ramakrishnan <mail.me.raghul87@gmail.com>
2024-11-07 18:07:36 -0500

    Moved all the Frontend files to frontend folder and enabled the python virtual env for backend works

---
+++ civic-tech-club/src/frontend/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';  // Your existing Home component
+import LondonBudget from './pages/Budgets/LondonBudget'; // The new About component
+import PDCalendar from './pages/PDCalendar/PDCalendar';
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/london-budget" element={<LondonBudget />} />
+        <Route path="/pd-calendar" element={<PDCalendar />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
