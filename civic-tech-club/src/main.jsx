0535d02e8bf32ec3175eda3641cbd269c70728d5
Raghul Ramakrishnan <mail.me.raghul87@gmail.com>
2024-11-07 17:45:14 -0500

    changed all files to jsx for consistency

--- civic-tech-club/src/main.jsx
+++ civic-tech-club/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../src/styles/style.scss'
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
