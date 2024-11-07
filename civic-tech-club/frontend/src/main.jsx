86e3d9d62e5a2c9ce54fd22390bf303515799f78
Raghul Ramakrishnan <mail.me.raghul87@gmail.com>
2024-11-07 18:23:53 -0500

    Moved src into frontend folder and created a new backend folder for flask

--- civic-tech-club/frontend/src/main.jsx
+++ civic-tech-club/frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../frontend/styles/style.scss'
+import './styles/style.scss'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
